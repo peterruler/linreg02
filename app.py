e410abfcfd8a574660365ea79c71ea714a39f61b
Peter Strössler <7starch@gmail.com>
2024-05-03 21:10:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 # Reload the model from the 2 files we saved
 with open('model_config.json') as json_file:
     json_config = json_file.read()
-flower_model = tf.keras.models.model_from_json(json_config)
-
+flower_model = model_from_json(json_config)
+# flower_model = tf.keras.models.model_from_json(json_config)
 # Load weights
 flower_model.load_weights('weights_only.h5')
 
