928cdf091de4f2ce8567b1442e57e7b5f4199286
Peter Strössler <7starch@gmail.com>
2024-05-03 23:34:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def return_prediction(model,scaler,sample_json):
     new_gem2 = [[feat1,feat2]]
     new_gem2 = scaler.transform(new_gem2)
     '''model = tensorflow.keras.models.load_model('my_model.h5')'''
-    return 1
+    return model.predict(new_gem2) 
     '''
     with graph.as_default():
         predict=model.predict(new_gem2) 
