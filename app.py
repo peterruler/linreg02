819be0be4e6057a3273f197c2f72625a8e116411
Peter Stroessler <peterstroessler@MacBook-Pro-von-Peter.local>
2024-07-20 14:25:30 +0200

    fresh project

--- app.py
+++ app.py
@@ -2,52 +2,29 @@ from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField
 from wtforms.validators import NumberRange
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import load_model  
 import numpy as np 
 import pickle
-from tensorflow.keras.backend import clear_session
 
 def return_prediction(model,scaler,sample_json):
-    clear_session()
-
-    graph = tf.get_default_graph()
     
     feat1 = sample_json['feat1']
     feat2 = sample_json['feat2']
-    print(f' {feat1}  {feat2}')
-    new_gem = [[feat1,feat2]]
-    new_gem = scaler.transform(new_gem)
     
-    print(f' {new_gem}')
-    return 1
+    new_gem2 = [[feat1,feat2]]
+    new_gem2 = scaler.transform(new_gem2)
     
-    '''
-    from tensorflow.keras.models import load_model
-    later_model = load_model('my_model.h5')
-    value = later_model.predict(new_gem)
+    predict=model.predict(new_gem2) 
     
-    with graph.as_default():
-        predict=model.predict(new_gem2) 
-        data = toDict(predict)
-    return flask.jsonify(data)
-    '''
+    return predict
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-# In tensorflow 1.10
-# Reload the model from the 2 files we saved
-with open('model_config.json') as json_file:
-    json_config = json_file.read()
-gem_model = model_from_json(json_config.encode('utf-8'))
-# Load weights
-gem_model.load_weights('weights_only.h5')
-
+flower_model = load_model("my_model.h5")
 scaler = pickle.load(open('scaler.sav', 'rb'))
 
-class GemForm(FlaskForm):
+class FlowerForm(FlaskForm):
     feat1 = StringField('Feature 1')
     feat2 = StringField('Feature 2')
 
@@ -56,7 +33,7 @@ class GemForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    form = GemForm()
+    form = FlowerForm()
     if form.validate_on_submit():
 
         session['feat1'] = form.feat1.data
@@ -74,10 +51,10 @@ def prediction():
     content['feat1'] = float(session['feat1'])
     content['feat2'] = float(session['feat2'])
 
-    results = return_prediction(model=gem_model,scaler=scaler,sample_json=content)
+    results = return_prediction(model=flower_model,scaler=scaler,sample_json=content)
 
-    return render_template('prediction.html',results=results)
+    return render_template('prediction.html',results=results[0][0])
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(debug=True,host='0.0.0.0', port=5000) 
